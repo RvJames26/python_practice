520c3b6c6333b8f0f9105d9f202ec20d8bf16043
RvJames26 <rovelljamesmariano05@gmail.com>
2026-03-08 17:01:50 +0800

    finished 04

---
+++ batch_2/prog_04.py
@@ -0,0 +1,5 @@
+#Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+#Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+num_01 = int(input("Input your first number: "))
+num_02 = int(input("Input your second number: "))
+print(f"Product: {num_01 // num_02}")
