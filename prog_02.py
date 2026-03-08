ab0aa868d0f35d63a3b9bb5d1a819268f294193d
Rovsss <113243254+RvJames26@users.noreply.github.com>
2026-03-08 16:32:33 +0800

    deleted files for better organization

--- prog_02.py
+++
@@ -1,7 +0,0 @@
-#Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
-num_01 = float(input("Input your first number: "))
-num_02 = float(input("Input your second number: "))
-if num_01 == num_02:
-    print(f"Equal")
-if num_01 != num_02:
-    print(f"Not Equal")
--- prog_04.py
+++
@@ -1,4 +0,0 @@
-#Create a program that ask user to input 2 numbers. Print the product of the two numbers.
-num_01 = float(input("Input your first number: "))
-num_02 = float(input("Input your second number: "))
-print(f"Product: {num_01 * num_02}")
