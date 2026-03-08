f8a3e7826e00255a8d580580330bcf3723bbca5b
RvJames26 <rovelljamesmariano05@gmail.com>
2026-03-08 17:08:38 +0800

    forgot to upload prog 06 in batch 1

---
+++ batch_1/prog_06.py
@@ -0,0 +1,4 @@
+#Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+num_01 = int(input("Input your first number: "))
+num_02 = int(input("Input your second number: "))
+print(f"{num_01 ** num_02}")
