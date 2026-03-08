89fc9be4bc47f05e9b1c3e29c3bd94b860c75b65
RvJames26 <rovelljamesmariano05@gmail.com>
2026-03-08 18:07:56 +0800

    added list in the start

--- batch_3/prog_01.py
+++ batch_3/prog_01.py
@@ -1,8 +1,6 @@
 #Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
-for i in range(0,10):
-    numbers = float(input("Enter number:"))
-    for check_numbers in numbers:
-        if numbers != numbers:
-            print(numbers)
-        if numbers == numbers:
-            continue
+num_set = []
+for i in range(0,2):
+    numbers = (input("Enter number:"))
+    list.numbers
+    print(numbers)
