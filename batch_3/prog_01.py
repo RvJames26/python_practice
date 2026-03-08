62066465cd336c5499648337b79a706aaa85d7f4
RvJames26 <rovelljamesmariano05@gmail.com>
2026-03-08 18:14:26 +0800

    tried set

--- batch_3/prog_01.py
+++ batch_3/prog_01.py
@@ -1,6 +1,8 @@
 #Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
-num_set = []
+num_entered = []
+duplicates = []
 for i in range(0,2):
     numbers = (input("Enter number:"))
-    list.numbers
-    print(numbers)
+    if numbers in num_entered:
+        numbers.append(num_entered)
+print(num_entered)
