ea1fc0465a3eda716de011f3c5244d30199bd6d9
RvJames26 <rovelljamesmariano05@gmail.com>
2026-03-08 15:29:55 +0800

    finished 09

--- prog_09.py
+++ prog_09.py
@@ -1,5 +1,8 @@
 #Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
-
-for i in range(1,100):
-    number = 0
-    print({number % 2 == 0})
+number = 0
+for i in range(1,101):
+    number += 1
+    if number % 2 == 0:
+        print(number, end=",")
+    if number % 2 == 1:
+        continue
