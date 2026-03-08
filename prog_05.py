163908d4c2855143d4dc4d783c61cd8dd42febfb
RvJames26 <rovelljamesmariano05@gmail.com>
2026-03-08 14:45:12 +0800

    finished 05

---
+++ prog_05.py
@@ -0,0 +1,4 @@
+#Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+num_01 = float(input("Input your first number: "))
+num_02 = float(input("Input your second number: "))
+print(f"Sum: {num_01 / num_02}")
