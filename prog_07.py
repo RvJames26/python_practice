fd754f0f4171f89602a685a8885f66b35bd871e6
RvJames26 <rovelljamesmariano05@gmail.com>
2026-03-08 15:06:22 +0800

    finished 07

--- prog_07.py
+++ prog_07.py
@@ -1,3 +1,6 @@
 #Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
-for count_number in range (0,10):
+start = 0
+for i in range (0,10):
     numbers = float(input("Enter number:")) 
+    start += numbers
+    print(start)
