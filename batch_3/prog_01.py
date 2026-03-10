d39b9a1fa745bd245174a2283dc793466cbdf18b
RvJames26 <rovelljamesmariano05@gmail.com>
2026-03-10 20:06:05 +0800

    trying to fix

--- batch_3/prog_01.py
+++ batch_3/prog_01.py
@@ -3,8 +3,9 @@ num_set = []
 final_number = []
 for i in range(0,10):
     numbers = int(input("Enter number:"))
-    for i in final_number:
-        if final_number.count(i) >= 1:
-            print(i, end=",")
-            continue
+    final_number.append(numbers)
+for i in final_number:
+    if final_number.count(i) >= 1:
+        print(i, end=",")
+        continue
 print(num_set)
