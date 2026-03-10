384841686bc9741902b134dfa5c2d2e1c29302dd
RvJames26 <rovelljamesmariano05@gmail.com>
2026-03-10 20:29:00 +0800

    created the try and except

---
+++ batch_3/prog_04.py
@@ -0,0 +1,5 @@
+#Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
+number_range = 0
+number_range += 1
+while True:
+    numbers_given = float(input("Type your numbers: "))
