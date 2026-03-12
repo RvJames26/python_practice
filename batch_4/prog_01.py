1516f322b57926ce977c21366697a287e29bc2e8
RvJames26 <rovelljamesmariano05@gmail.com>
2026-03-12 21:19:37 +0800

    finished 01

---
+++ batch_4/prog_01.py
@@ -0,0 +1,12 @@
+#Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
+num_set = []
+final_number = []
+for i in range(0,10):
+    numbers = int(input("Enter number:"))
+    final_number.append(numbers)
+for i in final_number:
+    if final_number.count(i) <= 1:
+        continue
+    if final_number.count(i) > 1:
+        print(i, end=",")
+        continue
