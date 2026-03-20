8f5725ec4f05e9a35def71097fdeeb9d39471703
RvJames26 <rovelljamesmariano05@gmail.com>
2026-03-20 23:40:27 +0800

    finished 04

---
+++ batch_5/prog_04.py
@@ -0,0 +1,8 @@
+#Create a program that ask the user to input their fullname. Print the input in all lower case.
+# Example:
+# Input: Juan Dela Cruz
+# Output: juan dela cruz
+
+name = input("Please input your full name: ")
+upper_case = name.lower()
+print(upper_case)
