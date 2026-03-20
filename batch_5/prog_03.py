1178190ecc359cfa349f1827de5df1a66ce1bc27
RvJames26 <rovelljamesmariano05@gmail.com>
2026-03-20 23:38:23 +0800

    finished 05

---
+++ batch_5/prog_03.py
@@ -0,0 +1,8 @@
+#Create a program that ask the user to input their fullname. Print the input in all capital letter.
+# Example:
+# Input: Juan Dela Cruz
+# Output: JUAN DELA CRUZ
+
+name = input("Please input your full name: ")
+upper_case = name.upper()
+print(upper_case)
